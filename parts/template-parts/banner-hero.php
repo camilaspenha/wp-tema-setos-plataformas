5673f354d25c892a9d74946fe1514af28d9b78c3
Camila Silva da Penha <camila@MacBook-Air-de-Camila.local>
2024-06-18 17:40:30 -0300

    style: banner hero

--- parts/template-parts/banner-hero.php
+++ parts/template-parts/banner-hero.php
@@ -5,8 +5,7 @@
       <div class="col-lg-6 d-flex text-start align-items-center">
         <div>
           <h1><?= $args['banner_hero_title'] ?></h1>
-          <h2><?= $args['banner_hero_subtitle'] ?></h2>
-          <h5><?= $args['banner_hero_description'] ?></h5>
+          <h2 class="mt-4" ><?= $args['banner_hero_subtitle'] ?>!<br /><?= $args['banner_hero_description'] ?></h2>
           <a href="#about" class="get-started-btn scrollto my-5"> Saiba Mais</a>
         </div>
       </div>
